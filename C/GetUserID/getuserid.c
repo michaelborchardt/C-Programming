9ed88da599d2a2d38cdf2ac57be8bcd77623020a
michaelborchardt <162625221+michaelborchardt@users.noreply.github.com>
2024-03-08 15:22:43 -0700

    Initial commit.
    Getuserid.c

---
+++ C/GetUserID/getuserid.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <pwd.h>
+
+/* Author: Michael A. Borchardt-Robertson
+ * Date: March 2nd, 2024
+*/
+
+int main(int argc, char *argv[]) {
+	uid_t	uid;
+	struct passwd *pwd;
+
+	uid = getuid();
+	printf("User's UID is %d.\n", (int)uid);
+	if (!(pwd = getpwuid(uid))) {
+		printf("Unable to get user's password file record!\n");
+		endpwent();
+		return 1;
+	}
+	printf("User's home directory is %s\n", pwd->pw_dir);
+	endpwent();
+
+	return 0;
+}
